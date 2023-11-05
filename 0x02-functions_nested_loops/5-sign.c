da8ce4999daddacad786c401fef2fdd1e6d5cd18
amr-ak <133274775+amr-ak@users.noreply.github.com>
2023-11-05 11:43:01 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * print_sign - prints the sign of a number
@@ -8,32 +8,18 @@ include "main.h"
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
-}
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
     else
     {
-        _putchar('-');
-        return (-1);
-    }
-}	{
-		_putchar('-');
-		return (-1);
-	}
-}
         _putchar('-');
         return (-1);
     }
