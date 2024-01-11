959c5dec9c8a92363e510ee08bfe60b8d8a0c9cb
amr-ak <amr.ben.khamis@gmail.com>
2024-01-11 20:42:41 +0200

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,46 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: number of bytes to copy from s2
- * Return: pointer to the concatenated string
+ * @s1: pointer to first string.
+ * @s2: pointer to 2nd string.
+ * @n: number of bytes from n2 to concatenate.
+ *
+ * Return: pointer to space in memory containing concatenated string.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *concat;
-    unsigned int i, j, len1, len2;
+	char *str;
+	unsigned int i, j, s1_length, s2_length;
 
-    /* check for NULL strings */
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	/*Check if the strings passed are NULL*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	/*Compute the length of the strings*/
 
-    /* calculate the lengths of the strings */
-    for (len1 = 0; s1[len1]; len1++)
-        ;
-    for (len2 = 0; s2[len2]; len2++)
-        ;
+	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
+		;
+	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
+		;
+	/*Memory reservation-for case 1 & 2*/
+	str = malloc(s1_length + n + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	/*Copy first string into str*/
+	for (i = 0; s1[i] != '\0'; 1++)
+		str[i] = s1[i];
+	/*Copy second string into str*/
+	for (j = 0; j < n; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
 
-    /* allocate memory for the concatenated string */
-    if (n >= len2) /* use the entire s2 */
-        concat = malloc(sizeof(char) * (len1 + len2 + 1));
-    else /* use only n bytes of s2 */
-        concat = malloc(sizeof(char) * (len1 + n + 1));
-
-    if (concat == NULL) /* check for memory allocation failure */
-        return (NULL);
-
-    /* copy the strings to the concatenated string */
-    for (i = 0; i < len1; i++) /* copy s1 */
-        concat[i] = s1[i];
-    for (j = 0; j < n && s2[j]; j++) /* copy n bytes of s2 */
-        concat[i + j] = s2[j];
-    concat[i + j] = '\0'; /* add null terminator */
-
-    return (concat); /* return the pointer to the concatenated string */
+	str[i] = '\0';
+	return (str);
 }
